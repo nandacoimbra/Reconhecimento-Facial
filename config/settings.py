d8aba51415b198f4ead5810e164ebb8507ac835c
Fernanda <fecoimbra96@gmail.com>
2025-11-09 23:00:08 -0300

    doc:  comentarios

--- config/settings.py
+++ config/settings.py
@@ -2,11 +2,11 @@ import os
 
 #SERIAL_PORT = '/dev/ttyUSB0'  # Linux
 #CAMERA_INDEX = 1  # Índice da câmera (0 para a primeira câmera conectada) linux
-#ENCODINGS_DIR = os.path.join('data', 'encodings') #diretorio linux
+#ENCODINGS_DIR = os.path.join('data', 'encodings') #diretorio para encodings no linux
 
 CAMERA_INDEX = 0  # Índice da câmera (0 para a primeira câmera conectada) windows
 SERIAL_PORT = 'COM7'  # ou 'COM3' no Windows
-ENCODINGS_DIR = 'data\encodings'
+ENCODINGS_DIR = 'data\encodings' #diretorio para encodings no windows
 
 # Diretório onde serão salvos os frames de acesso (rostos reconhecidos)
 ACCESS_DIR = os.path.join('data', 'access_frames')
